b73bacd02cd4c3ab8d393558546943a27ebe1d4d
Mutisomary <marymutso2016@gmail.com>
2024-01-09 06:42:05 +0000

    set data structure

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,8 @@
 export default function hasValuesFromArray(set, array) {
-	const hasElements = array.every((value) => set.has(value));
-	return hasElements;
+  for (let i = 0; i < array.length; i++) {
+    if (!set.has(array[i])) {
+      return false;
+    }
+  }
+  return true;
 }
