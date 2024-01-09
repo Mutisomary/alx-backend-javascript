6890cf8f17d1573297ba18a452324da50bd21670
Mutisomary <marymutso2016@gmail.com>
2024-01-09 04:31:07 +0000

    mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,7 @@
 export default function getListStudentIds(studentList) {
-  if (!Array.isArray(studentList)) {
-	return [];
-  } 
-  return studentList.map((student) => student.id);
+  if (Array.isArray(studentList)) {
+    const studentIds = studentList.map((student) => student.id);
+    return studentIds;
+  }
+  return [];
 }
